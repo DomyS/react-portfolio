16d5d5c0a8c8b0bcf394339ad14513ea79d24e44
Domante Stirbyte <domante.stirbyte@gmail.com>
2019-05-07 10:17:55 +0200

    Updates

---
+++ precache-manifest.5642c1b67ac08dc0e445b6d09c548175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f016f6223b8eab605d7dc7fe34e5e6e9",
+    "url": "/react-portfolio/static/media/portfolio.f016f622.jpg"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "ebb7a8d537e911943f28",
+    "url": "/react-portfolio/static/js/main.ebb7a8d5.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/react-portfolio/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "ebb7a8d537e911943f28",
+    "url": "/react-portfolio/static/css/main.29afdb66.chunk.css"
+  },
+  {
+    "revision": "7007fb46dae58331fcb1dec01ea0dad3",
+    "url": "/react-portfolio/index.html"
+  }
+];
